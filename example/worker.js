65bec4b8785db9d233fcf7d0395ab38270e2ea20
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-06 12:39:59 +0100

    example: update web worker

--- example/worker.js
+++ example/worker.js
@@ -4,8 +4,9 @@ self.addEventListener('message', (e) => {
   const { config, source } = e.data
 
   const filter = dither[config.nature]
+  const lookup = dither.matrix[config.lookup]
 
   self.postMessage({
-    result: filter(config.lookup)(source)
+    result: filter(lookup)(source)
   })
 })
