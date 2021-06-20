c62e2d15492e41d6574884460956311c094c9904
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-20 19:30:11 +0100

    package: use esbuild to bundle for the example

--- example/dither.js
+++ example/dither.js
@@ -1,11 +1,35 @@
-(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (global = global || self, factory(global.dither = {}));
-}(this, (function (exports) { 'use strict';
+var dither = (() => {
+  var __defProp = Object.defineProperty;
+  var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+  var __export = (target, all) => {
+    __markAsModule(target);
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
 
-  // Spatial (error diffusion)
-  const atkinson = [
+  // main.js
+  var main_exports = {};
+  __export(main_exports, {
+    matrix: () => matrix_exports,
+    ordered: () => ordered,
+    spatial: () => spatial
+  });
+
+  // matrix.js
+  var matrix_exports = {};
+  __export(matrix_exports, {
+    atkinson: () => atkinson,
+    bayer16: () => bayer16,
+    bayer64: () => bayer64,
+    burkes: () => burkes,
+    floydSteinberg: () => floydSteinberg,
+    jarvisJudiceNinke: () => jarvisJudiceNinke,
+    sierra2: () => sierra2,
+    sierra3: () => sierra3,
+    sierraLite: () => sierraLite,
+    stucki: () => stucki
+  });
+  var atkinson = [
     {
       x: 1,
       y: 0,
@@ -37,8 +61,7 @@
       weight: 1 / 8
     }
   ];
-
-  const burkes = [
+  var burkes = [
     {
       x: 1,
       y: 0,
@@ -75,8 +98,7 @@
       weight: 2 / 32
     }
   ];
-
-  const floydSteinberg = [
+  var floydSteinberg = [
     {
       x: 1,
       y: 0,
@@ -98,8 +120,7 @@
       weight: 1 / 16
     }
   ];
-
-  const jarvisJudiceNinke = [
+  var jarvisJudiceNinke = [
     {
       x: 1,
       y: 0,
@@ -161,8 +182,7 @@
       weight: 1 / 48
     }
   ];
-
-  const sierra2 = [
+  var sierra2 = [
     {
       x: 1,
       y: 0,
@@ -199,8 +219,7 @@
       weight: 1 / 16
     }
   ];
-
-  const sierra3 = [
+  var sierra3 = [
     {
       x: 1,
       y: 0,
@@ -252,8 +271,7 @@
       weight: 2 / 32
     }
   ];
-
-  const sierraLite = [
+  var sierraLite = [
     {
       x: 1,
       y: 0,
@@ -270,8 +288,7 @@
       weight: 1 / 4
     }
   ];
-
-  const stucki = [
+  var stucki = [
     {
       x: 1,
       y: 0,
@@ -333,114 +350,136 @@
       weight: 1 / 42
     }
   ];
-
-  // Ordered (threshold)
-  const bayer16 = [
-    0, 8, 2, 10,
-    12, 4, 14, 6,
-    3, 11, 1, 9,
-    15, 7, 13, 5
+  var bayer16 = [
+    0,
+    8,
+    2,
+    10,
+    12,
+    4,
+    14,
+    6,
+    3,
+    11,
+    1,
+    9,
+    15,
+    7,
+    13,
+    5
+  ];
+  var bayer64 = [
+    0,
+    48,
+    12,
+    60,
+    3,
+    51,
+    15,
+    63,
+    32,
+    16,
+    44,
+    28,
+    35,
+    19,
+    47,
+    31,
+    8,
+    56,
+    4,
+    52,
+    11,
+    59,
+    7,
+    55,
+    40,
+    24,
+    36,
+    20,
+    43,
+    27,
+    39,
+    23,
+    2,
+    50,
+    14,
+    62,
+    1,
+    49,
+    13,
+    61,
+    34,
+    18,
+    46,
+    30,
+    33,
+    17,
+    45,
+    29,
+    10,
+    58,
+    6,
+    54,
+    9,
+    57,
+    5,
+    53,
+    42,
+    26,
+    38,
+    22,
+    41,
+    25,
+    37,
+    21
   ];
 
-  const bayer64 = [
-    0, 48, 12, 60, 3, 51, 15, 63,
-    32, 16, 44, 28, 35, 19, 47, 31,
-    8, 56, 4, 52, 11, 59, 7, 55,
-    40, 24, 36, 20, 43, 27, 39, 23,
-    2, 50, 14, 62, 1, 49, 13, 61,
-    34, 18, 46, 30, 33, 17, 45, 29,
-    10, 58, 6, 54, 9, 57, 5, 53,
-    42, 26, 38, 22, 41, 25, 37, 21
-  ];
-
-  var matrix = /*#__PURE__*/Object.freeze({
-    __proto__: null,
-    atkinson: atkinson,
-    burkes: burkes,
-    floydSteinberg: floydSteinberg,
-    jarvisJudiceNinke: jarvisJudiceNinke,
-    sierra2: sierra2,
-    sierra3: sierra3,
-    sierraLite: sierraLite,
-    stucki: stucki,
-    bayer16: bayer16,
-    bayer64: bayer64
-  });
-
-  const ordered = (table = bayer64) => {
+  // main.js
+  var ordered = (table = bayer64) => {
     const steps = Math.sqrt(table.length);
     const scale = table.length / 255;
-
     return (input = { data: [], width: 0 }) => {
       const frame = new Uint8ClampedArray(input.data.buffer);
       const width = input.width;
-
       for (let i = 0, stop = frame.length; i < stop; i += 4) {
         const r = frame[i];
         const g = frame[i + 1];
         const b = frame[i + 2];
-
         const color = [r, g, b];
-
         const j = i * 0.25;
-
         const x = j % steps;
         const y = Math.floor(j / width) % steps;
-
-        const limit = table[(x * steps) + y];
-        const pixel = color.map(v => (v * scale > limit ? 255 : 0));
-
+        const limit = table[x * steps + y];
+        const pixel = color.map((v) => v * scale > limit ? 255 : 0);
         frame.set(pixel, i);
       }
-
-      return input
-    }
+      return input;
+    };
   };
-
-  const spatial = (model = floydSteinberg) => {
+  var spatial = (model = floydSteinberg) => {
     const steps = model.length * 3;
-
     return (input = { data: [], width: 0 }) => {
       const { data, width } = input;
-
-      // Reformat lookup table
-      const table = model.map(v => (v.x * 4) + (v.y * 4 * width));
-
+      const table = model.map((v) => v.x * 4 + v.y * 4 * width);
       for (let i = 0, stop = data.length; i < stop; i += 4) {
         const r = data[i];
         const g = data[i + 1];
         const b = data[i + 2];
-
         const color = [r, g, b];
-
-        // Quantize (8 color palette)
-        const pixel = color.map(v => (v > 127 ? 255 : 0));
-
-        // Compute error
-        const error = color.map(v => (v > 127 ? v - 255 : v));
-
-        // Diffuse error
+        const pixel = color.map((v) => v > 127 ? 255 : 0);
+        const error = color.map((v) => v > 127 ? v - 255 : v);
         for (let j = 0; j < steps; j += 1) {
           const x = j % 3;
           const y = Math.floor(j / 3);
-
           const e = error[x] * model[y].weight;
           const k = table[y] + x + i;
-
           data[k] += Math.floor(e);
         }
-
         data.set(pixel, i);
       }
-
-      return input
-    }
+      return input;
+    };
   };
-
-  exports.matrix = matrix;
-  exports.ordered = ordered;
-  exports.spatial = spatial;
-
-  Object.defineProperty(exports, '__esModule', { value: true });
-
-})));
+  return main_exports;
+})();
